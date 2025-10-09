d1793afe59842fc404e1c9365d2cfbc77cbbc0e7
devdanizin <devdanielprof@gmail.com>
2025-10-09 15:38:53 -0300

    Creat a API.

---
+++ src/test/java/com/devdaniel/MarathonAPI/MarathonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdaniel.MarathonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
